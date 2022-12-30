c10a647faa744c7ae3a3d2191e1133e20a0342f8
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2022-12-30 23:01:34 +0700

    Update Ham_kiemtra_tamgiac_vuong.c

--- Ham_kiemtra_tamgiac_vuong.c
+++ Ham_kiemtra_tamgiac_vuong.c
@@ -1,8 +1,3 @@
-// Ho ten : Nguyễn Văn Tuấn
-//MSV: B20DCPT182
-//Nhóm lớp: 19
-//STT: 19
-//Bai so LQ08
 #include<stdio.h>
 float  check(float a, float b, float c){
 	if(a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b)
